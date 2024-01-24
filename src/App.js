2c742dc29008fceb99e4c9a49aa10df3ab0dfe29
m9zjl <i667zjl@hotmail.com>
2024-01-24 22:05:17 +0800

    append

--- src/App.js
+++ src/App.js
@@ -16,15 +16,38 @@ export default function Board() {
 
     const [squares, setSquares] = useState(Array(9).fill(null))
 
-    function ()=> handleClick(index) {
+    const [xIsNext, setXIsNext] = useState(true);
+
+    function handleClick(i) {
         const nextSquares = squares.slice();
-        nextSquares[index] = 'X';
+
+
+        if (calculateWinner(squares) || squares[i]) {
+            console.log('duplicate actioin')
+            return;
+        }
+
+        if (xIsNext) {
+            nextSquares[i] = 'X';
+        } else {
+            nextSquares[i] = 'O';
+        }
         setSquares(nextSquares)
+        setXIsNext(!xIsNext)
     }
 
+    const winner = calculateWinner(squares);
+    let status;
+
+    if (winner) {
+        status = 'Winer: ' + winner;
+    } else {
+        status = 'Next Player: ' + (xIsNext ? 'X' : 'O')
+    }
 
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
@@ -42,4 +65,24 @@ export default function Board() {
             </div>
         </>
     )
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
 }
