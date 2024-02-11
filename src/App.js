e648a1c4270830f6b2ccd46749cf80f3a289d508
m9zjl <i667zjl@hotmail.com>
2024-02-11 13:07:43 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,75 @@
+import { useState } from "react";
 import { poeple } from "./data";
 import { getImageUrl } from "./utils";
 
-export default function List() {
+export default function Form() {
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('typing')
+
+  if (status === 'success') {
+    return <h1>That's right</h1>
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('submitting')
+    try {
+      await submitForm(answer);
+      setStatus('success');
+    } catch (err) {
+      setStatus('typing');
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+  }
+
+  function submitForm(answer) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let shouldError = answer.toLowerCase() !== 'lima';
+        if (shouldError) {
+          reject(new Error('wrong anser'));
+        } else {
+          resolve();
+        }
+      }, 1500)
+    })
+  }
+
+
+  return (
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is tere a billboard that turns air into drinkable water?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === 'submitting'}
+        />
+        <br />
+        <button disabled={answer.length === 0 || status === 'submitting'}>Submit</button>
+        {error !== null && <p className="Error">{error.message}</p>}
+      </form>
+    </>
+  )
+}
+
+
+function List() {
   const listItems = poeple.map(
     item => (
       <li key={item.id} >
         <img src={getImageUrl(item)}
           alt={item.id} />
         <p>
-          <b>{item.name}:</b><br/>
+          <b>{item.name}:</b><br />
           {' ' + item.profession + ' '}
           known for {item.accomplishment}
         </p>
