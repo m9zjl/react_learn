190c4e41f56d34dcab4f23f0e11dce8ce4fe6dbb
chances <zhangjiale.zjl@alibaba-inc.com>
2024-02-19 13:43:06 +0800

    server

--- src/Components/Blog/Write.jsx
+++ src/Components/Blog/Write.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 function Write() {
+  const [value, setValue] = useState("");
+
   return (
-    <>
-      <h1>Write</h1>
-    </>
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill theme="snow" value={value} onChange={setValue} />;
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">item1</div>
+        <div className="item">item2</div>
+      </div>
+    </div>
   );
 }
 
